7870d646377311f634cfc1dadeeba4cc33d8eb7d
Bao Khoa <khoatptk1@gmail.com>
2024-04-17 15:39:04 +0700

    w11

--- paths.py
+++ paths.py
@@ -1,7 +1,7 @@
 import json
-from utils.fileProcessor import dataReader
 from classes.Path import *
-
+from utils.fileProcessor import dataReader
+from utils.promptExtract import promptExtract
 INPUT_FILENAME = "paths.json"
 OUTPUT_FILENAME_CSV = "paths.csv"
 OUTPUT_FILENAME_JSON = "paths.json"
@@ -9,18 +9,25 @@ OUTPUT_FILENAME_JSON = "paths.json"
 routesGroup = dataReader(INPUT_FILENAME)
 
 def getListPath():
-  res = list()
-  for routes in routesGroup:
-    if not routes:
-      continue
-    routes = json.loads(routes)
-    res.append(Path(routes))
-    
-  listPath = PathQuery(res)
-  return listPath
+    res = list()
+    for routes in routesGroup:
+      if not routes:
+        continue
+      routes = json.loads(routes)
+      res.append(Path(routes))
+      
+    listPath = PathQuery(res)
+    return listPath
 
 if __name__ == "__main__":
-  path = getListPath()
-  data = path.searchByLngLat(106.652565, 10.75125313)
-  path.outputAsCSV(OUTPUT_FILENAME_CSV, data)
-  path.outputAsJSON(OUTPUT_FILENAME_JSON, data)
+    path = getListPath()
+    prompt = input("Enter the prompt: ")
+    result = promptExtract(prompt, pathKeys) #, lng = 106.652565, lat = 10.75125313
+    if result is not None:
+        field, value, function = result
+        chosenFunction = eval(f"path.{function}")
+        data = chosenFunction(field, value)
+        path.outputAsCSV(OUTPUT_FILENAME_CSV, data)
+        path.outputAsJSON(OUTPUT_FILENAME_JSON, data)
+    else :
+        print("No result found, please try again.")
--- stops.py
+++ stops.py
@@ -1,7 +1,8 @@
 import json
 
-from utils.fileProcessor import *
 from classes.Stop import *
+from utils.fileProcessor import *
+from utils.promptExtract import promptExtract
 
 INPUT_FILENAME = "stops.json"
 OUTPUT_FILENAME_CSV = "stops.csv"
@@ -9,18 +10,25 @@ OUTPUT_FILENAME_JSON = "stops.json"
 stopsGroup = dataReader(INPUT_FILENAME)
 
 def getListStop():
-  res = list()
-  for stops in stopsGroup:
-    if not stops:
-      continue
-    stops = json.loads(stops)
-    res.append(Stop(stops))
-  
-  listRoutes = StopQuery(res)
-  return listRoutes
+    res = list()
+    for stops in stopsGroup:
+        if not stops:
+            continue
+        stops = json.loads(stops)
+        res.append(Stop(stops))
+    
+    listRoutes = StopQuery(res)
+    return listRoutes
 
 if __name__ == "__main__": 
-  stop = getListStop()
-  data = stop.searchByAttr(StopId=35)
-  stop.outputAsCSV(OUTPUT_FILENAME_CSV, data)
-  stop.outputAsJSON(OUTPUT_FILENAME_JSON, data)
+    stop = getListStop()
+    prompt = input("Enter the prompt: ")
+    result = promptExtract(prompt, stopKeys)
+    if result is not None:
+        field, value, function = result
+        chosenFunction = eval(f"stop.{function}")
+        data = chosenFunction(field, value)
+        stop.outputAsCSV(OUTPUT_FILENAME_CSV, data)
+        stop.outputAsJSON(OUTPUT_FILENAME_JSON, data)
+    else :
+        print("No result found, please try again.")
